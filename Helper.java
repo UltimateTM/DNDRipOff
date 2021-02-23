7e074029ec8e5fa7da4d91f6aa7b5f7c6863dcab
Gabriel Limberg <gabriellimberg@gmail.com>
2021-02-23 23:39:46 +0000

    Removed .class files

--- Helper.java
+++ Helper.java
@@ -5,7 +5,7 @@ public class Helper {
 
   	public ArrayList<Character> characters = new ArrayList<Character>();
 
-    public ArrayList<Items> items = new ArrayList<Items>();
+    public ArrayList<Item> items = new ArrayList<Item>();
 
 	public Helper() {
 
